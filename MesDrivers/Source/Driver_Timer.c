ab5fbb658a694fd9b7eaa3041c68072a94cb060e
mstrdav <colin.geindre@gmail.com>
2023-10-04 11:09:20 +0200

    driver ready ?

--- MesDrivers/Source/Driver_Timer.c
+++ MesDrivers/Source/Driver_Timer.c
@@ -2,6 +2,9 @@
 
 void MyTimer_Base_Init(TIM_TypeDef * Timer, unsigned short ARR, unsigned short PSC)
 {
+    // Enable the clock for the timer
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+
     Timer->ARR = ARR;
     Timer->PSC = PSC;
     Timer->EGR |= TIM_EGR_UG;
